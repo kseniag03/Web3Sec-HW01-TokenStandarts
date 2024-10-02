b48620599c49f4ae4c3ce27c413fd9bd8519651b
kseniag03 <114473740+kseniag03@users.noreply.github.com>
2024-10-02 18:54:34 +0300

    added overrided transfer for erc20

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -7,13 +7,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract ERC20Token is ERC20, Ownable, ERC20Permit {
+    address public feeCollector;
+    uint256 public feePercent;
+
     constructor(
-        address initialOwner
+        address initialOwner,
+        address _feeCollector,
+        uint256 _feePercent
     )
         ERC20("ERC20Token", "MTK")
         Ownable(initialOwner)
         ERC20Permit("ERC20Token")
-    {}
+    {
+        feeCollector = _feeCollector;
+        feePercent = _feePercent;
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
@@ -33,5 +41,40 @@ contract ERC20Token is ERC20, Ownable, ERC20Permit {
         if (msg.value > totalCost) {
             payable(msg.sender).transfer(msg.value - totalCost);
         }
+    }
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        uint256 fee = (amount * feePercent) / 100;
+        uint256 amountFee = amount - fee;
+
+        require(
+            balanceOf(msg.sender) >= amount,
+            "Insufficient funds on balance"
+        );
+
+        super.transfer(feeCollector, fee);
+        super.transfer(recipient, amountFee);
+
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        uint256 fee = (amount * feePercent) / 100;
+        uint256 amountFee = amount - fee;
+
+        require(balanceOf(sender) >= amount, "Insufficient funds on balance");
+        require(allowance(sender, msg.sender) >= amount, "Allowance exceeded");
+
+        super.transferFrom(sender, feeCollector, fee);
+        super.transferFrom(sender, recipient, amountFee);
+
+        return true;
     }
 }
